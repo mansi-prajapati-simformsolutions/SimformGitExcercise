8cadf7a37c1dc9ea073229a455e9a8ba7774ecd2
mansiprajapati169 <mansiprajapati169@gmail.com>
2022-10-13 12:15:51 +0530

    Login screen completed

---
+++ app/src/main/java/simform/gitexcercise/android/LoginActivity.kt
@@ -0,0 +1,29 @@
+package simform.gitexcercise.android
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import simform.gitexcercise.android.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setData()
+    }
+
+    private fun setData() {
+        binding.tvGoToSignup.setOnClickListener {
+            val intent = Intent(applicationContext, RegistrationActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.tvForgotPassword.setOnClickListener {
+            val intent = Intent(applicationContext, ForgotPasswordActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
